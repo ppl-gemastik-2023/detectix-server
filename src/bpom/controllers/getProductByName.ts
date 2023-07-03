bbe7c42524a3ddda533a0c56e7adbf0dd3752f1e
juankhusuma <juan.d.khusuma@gmail.com>
2023-07-03 19:17:49 +0700

    added bpom search by name feature

---
+++ src/bpom/controllers/getProductByName.ts
@@ -0,0 +1,33 @@
+import { Request, Response } from "express";
+import { sendBPOMRequest } from "../../lib/sendBPOMRequest";
+import { Product } from "../../types/product";
+
+interface SearchResult {
+    arrproc: string;
+    from_home_flag: string;
+    kirimanfilter: Kirimanfilter;
+    offset: number;
+    next_prev: number;
+    data_all_produk: Product[];
+    count_data_all_produk: CountDataAllProduk;
+}
+
+interface CountDataAllProduk {
+    JUMLAH: number;
+}
+
+interface Kirimanfilter {
+    st_filter: string;
+    input_search: string;
+    from_home_flag: string;
+}
+
+
+export async function searchByName(req: Request, res: Response) {
+    const { q } = req.query;
+    const result = await sendBPOMRequest<SearchResult>("https://cekbpom.pom.go.id/search_all_produk", {
+        st_filter: "2",
+        input_search: q,
+    });
+    res.json(result.data_all_produk)
+}
--- src/bpom/routes.ts
+++ src/bpom/routes.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import { getProductById } from "./controllers/getProductById";
 import { getRecalledProductById } from "./controllers/getRecalledProductById";
+import { searchByName } from "./controllers/getProductByName";
 
 export const BPOMRouter = express.Router()
     .get('/products/:id', getProductById)
     .get('/recalled/:id', getRecalledProductById)
+    .get('/search', searchByName)
